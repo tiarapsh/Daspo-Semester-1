4afea86c1ecbf116c665bff316757a120bf8307b
tiara <haiiiinirara@gmail.com>
2025-11-13 16:50:30 +0700

    fix array

--- Jobsheet11/NestedLoop22.java
+++ Jobsheet11/NestedLoop22.java
@@ -5,18 +5,18 @@ import java.util.Scanner;
 public class NestedLoop22 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        double temps[][] =  {{5, 7}, {5, 7}};
-        for (int i = 0; i < temps.length; i++){
-            System.out.println("City " +i);
-            for ( int j =0; j < temps[0].length; j++){
-                System.out.println("Day " + (j + 1) +": ");
-                temps[i][j] = scan.nextDouble(); 
+        double temps[][] = new double[5][7];
+        for (int i = 0; i < temps.length; i++) {
+            System.out.println("City " + i);
+            for (int j = 0; j < temps[0].length; j++) {
+                System.out.println("Day " + (j + 1) + ": ");
+                temps[i][j] = scan.nextDouble();
             }
             System.out.println();
         }
-        for ( int i =0;  i< temps.length; i++){
+        for (int i = 0; i < temps.length; i++) {
             System.out.println("City " + i);
-            for ( int j =0; j < temps[0].length; j++){
+            for (int j = 0; j < temps[0].length; j++) {
                 System.out.println(temps[i][j] + " ");
             }
             System.out.println();
