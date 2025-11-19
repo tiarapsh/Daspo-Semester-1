d34a505bcc337cad843810e24b6454b7ffb166ee
tiara <haiiiinirara@gmail.com>
2025-11-19 21:10:16 +0700

    added arraynumbers

---
+++ Jobsheet9/arrayNumbers22.java
@@ -0,0 +1,12 @@
+package Jobsheet9;
+
+public class arrayNumbers22 {
+    public static void main(String[] args) {
+        int[] num = new int[4];
+
+        num[0] = 5;
+        num[1] = 12;
+        num[2] = 7;
+        num[3] = 20;
+    }
+}
