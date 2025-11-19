34870ed904c38001c0f727690e04e45b63c9af81
tiara <haiiiinirara@gmail.com>
2025-11-19 23:45:44 +0700

    task 1

--- Jobsheet9/arrayValue22.java
+++ Jobsheet9/arrayValue22.java
@@ -7,12 +7,17 @@ public class arrayValue22 {
         Scanner scan = new java.util.Scanner(System.in);
         int [] finalScore = new int [10];
 
-        for (int i = 0; i < 10; i++){
-            System.out.println("Enter Final Score " + i+ ": ");
-            finalScore[i]= scan.nextInt();
+        for (int i = 0; i < finalScore.length; i++) {
+            System.out.print("Enter the final score " + i + ": ");
+            finalScore[i] = scan.nextInt();
         }
-        for (int i= 0; i < 10; i++){
-            System.out.println("Student " + i + " is " + finalScore[i]);
+
+        for (int i = 0; i < finalScore.length; i++) {
+            if (finalScore[i] > 70) {
+                System.out.println("Student " + i + " Passed!");
+            } else {
+                System.out.println("Student " + i + " Failed!");
+            }
         }
     }
 }
