332081f9b7d362824d827a9a5e05210f01f2906a
tiara <haiiiinirara@gmail.com>
2025-11-19 21:37:10 +0700

    modified (2)

--- Jobsheet9/arrayNumbers22.java
+++ Jobsheet9/arrayNumbers22.java
@@ -2,11 +2,11 @@ package Jobsheet9;
 
 public class arrayNumbers22 {
     public static void main(String[] args) {
-           int[] num = {5, 12, 7, 20};
+        int[] num = { 5, 12, 7, 20 };
 
-       System.out.println(num[0]);
-       System.out.println(num[1]);
-       System.out.println(num[2]);
-       System.out.println(num[3]);
+        for (int i = 0; i < 4; i++) {
+            System.out.println(num[i]);
+        }
+
     }
 }
