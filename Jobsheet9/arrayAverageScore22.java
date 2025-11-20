6278398134dd0a3457b343963112c6f0365533df
tiara <haiiiinirara@gmail.com>
2025-11-20 14:45:38 +0700

    modified average score

--- Jobsheet9/arrayAverageScore22.java
+++ Jobsheet9/arrayAverageScore22.java
@@ -5,19 +5,42 @@ import java.util.Scanner;
 public class arrayAverageScore22 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int[] score = new int[10];
+        
         double total = 0;
         double average;
+        int passedCount = 0;
+        int failedCount = 0;
+        double totalPassed = 0;
+        double totalFailed = 0;
+        
+        System.out.print("Enter the number of students: ");
+        int numStudents = scan.nextInt();
+        int[] score = new int[numStudents];
 
-        for (int i= 0; i < score.length; i++) {
-            System.out.print("Enter student score " + (i+1) + ":");
+        for (int i = 0; i < score.length; i++) {
+            System.out.print("Enter student score " + i + ":");
             score[i] = scan.nextInt();
         }
 
-        for (int i = 0; i < score.length; i++){
+        for (int i = 0; i < score.length; i++) {
+            if (score[i] > 70) {
+                // System.out.println("Student " + i + " Passed!");
+                passedCount++;
+                totalPassed += score[i];
+            } else {
+                // System.out.println("Student " + i + " Failed!");
+                failedCount++;
+                totalFailed += score[i];
+            }
+        }
+        for (int i = 0; i < score.length; i++) {
             total += score[i];
         }
         average = total / score.length;
-        System.out.println("The class average score is: " + average);
+        double averagePassed = totalPassed / passedCount;
+        double averageFailed = totalFailed / failedCount;
+        
+        System.out.println("The average of students who passed is : " + averagePassed);
+        System.out.println("The average of students who failed is : " + averageFailed);
     }
 }
