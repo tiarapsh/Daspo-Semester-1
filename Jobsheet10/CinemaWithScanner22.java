c7dab51682dc6f97e50291fc98d31f4a47f4c59b
tiara <haiiiinirara@gmail.com>
2025-11-22 01:01:38 +0700

    modified

--- Jobsheet10/CinemaWithScanner22.java
+++ Jobsheet10/CinemaWithScanner22.java
@@ -6,6 +6,8 @@ public class CinemaWithScanner22 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String[][] audience = new String[4][2];
+        int row = 0;
+        int column = 0;
 
         while (true) {
             System.out.println("\n=== Menu ===");
@@ -19,15 +21,27 @@ public class CinemaWithScanner22 {
             if (menu == 1) {
                 System.out.print("Enter name:");
                 String name = scan.nextLine();
-                System.out.print("Enter row number:");
-                int row = scan.nextInt();
-                System.out.print("Enter column number:");
-                int column = scan.nextInt();
-                scan.nextLine();
-                if (audience[row - 1][column - 1] != null) {
-                    System.out.println("Seat already taken! Please choose another seat.");
-                    continue;
+
+                while (true){
+
+                    System.out.print("Enter row number (1-4):");
+                    row = scan.nextInt();
+                    System.out.print("Enter column number(1-2):");
+                    column = scan.nextInt();
+                    scan.nextLine();
+
+                    if (row < 1 || row > 4 || column < 1 || column > 2) {
+                        System.out.println("Invalid seat position! Please enter a valid row (1-4) and column (1-2).");
+                        continue;
+                    } 
+
+                    if (audience[row - 1][column - 1] != null) {
+                        System.out.println("Seat already taken! Please choose another seat.");
+                        continue;
                 }
+                    break;
+                }
+
                 audience[row - 1][column - 1] = name;
                 System.out.println("Audience Added!");
 
@@ -36,7 +50,7 @@ public class CinemaWithScanner22 {
                 for (int i = 0; i < audience.length; i++) {
                     System.out.print("Row " + (i + 1) + ": ");
                     for (int j = 0; j < audience[i].length; j++) {
-                        System.out.print((audience[i][j] != null ? audience[i][j] : "-") + " ");
+                        System.out.print((audience[i][j] != null ? audience[i][j] : "***") + " ");
                     }
                     System.out.println();
                 }
