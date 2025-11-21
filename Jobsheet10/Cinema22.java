45a1f5ad358c73f0874d49f7becc076e538b90a8
tiara <haiiiinirara@gmail.com>
2025-11-21 21:45:31 +0700

    modify 11

--- Jobsheet10/Cinema22.java
+++ Jobsheet10/Cinema22.java
@@ -12,23 +12,12 @@ public class Cinema22 {
         audience[2][1] = "Farhan";
         audience[3][0] = "Gisel";
         audience[3][1] = "Hana";
-
-        // System.out.println(audience[0].length);
-        // System.out.println(audience[1].length);
-        // System.out.println(audience[2].length); 
-        // System.out.println(audience[3].length);
-        // System.out.println("====================");
-        // System.out.printf("%s \t %s\n", audience[0][0], audience[0][1]);
-        // System.out.printf("%s \t %s\n", audience[1][0], audience[1][1]);
-        // System.out.printf("%s \t %s\n", audience[2][0], audience[2][1]);
-        // System.out.printf("%s \t %s\n", audience[3][0], audience[3][1]);
         
         System.out.println(audience.length);
-        // for (int i = 0; i < audience.length; i++){
-        //     System.out.println("Length of row " + (i+1) + ": " + audience[i].length);
-        // }
-        for (String[] rowAudience : audience){
-            System.out.println("Length of row: " + rowAudience.length);
+   
+        System.out.println("Audience in row 3: ");
+        for (String i : audience[2]){
+            System.out.println(i);
         }
 
 
