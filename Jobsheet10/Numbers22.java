2ef3e9f9fc750f6f04e979712fa5411ef3ada758
tiara <haiiiinirara@gmail.com>
2025-11-22 12:39:48 +0700

    added modified

--- Jobsheet10/Numbers22.java
+++ Jobsheet10/Numbers22.java
@@ -1,5 +1,7 @@
 package Jobsheet10;
 
+import java.util.Arrays;
+
 public class Numbers22 {
     public static void main(String[] args) {
         int [][] myNumbers = new int [3][];
@@ -7,6 +9,13 @@ public class Numbers22 {
         myNumbers[1] = new int [3];
         myNumbers[2] = new int [1];
 
-        
+        for (int i = 0; i < myNumbers.length; i++){
+            System.out.println(Arrays.toString(myNumbers[i]));
+        }
+
+        for (int i = 0; i < myNumbers.length; i++){
+            System.out.println("Length of row:" + (i + 1) + ":" + myNumbers[i].length);
+        }
+
     }
 }
